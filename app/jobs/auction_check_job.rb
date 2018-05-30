c3f8f29b58211b960ded405560ffaff07d08a284
ecmerchant <ecmerchant.contact@gmail.com>
2018-05-31 08:09:53 +0900

    change pg version

--- app/jobs/auction_check_job.rb
+++ app/jobs/auction_check_job.rb
@@ -19,143 +19,201 @@ class AuctionCheckJob < ApplicationJob
     token = account.cw_api_token
     rid = account.cw_room_id
     std = Time.now.to_s
-    msg = "ヤフオク監視開始\n処理開始日時：" + std + "\n処理対象：" + tag.length.to_s + "件"
+    msg = "監視開始\n処理開始日時：" + std + "\n処理対象：" + tag.length.to_s + "件"
     logger.debug(msg)
     msend(msg, token, rid)
     tag.each do |sku|
-      url = 'https://page.auctions.yahoo.co.jp/jp/auction/' + sku
-      logger.debug(url)
-#      uanum = ua.length
-#      user_agent = ua[rand(uanum)][0]
-      charset = nil
-      rt = rand(10)*0.1+0.2
-      sleep(rt)
-      begin
-        logger.debug("Access URL")
-        request = Typhoeus::Request.new(url)
-        request.run
-        html = request.response.body
-        doc = Nokogiri::HTML.parse(html, nil, charset)
-        isValid = true
-        auction = doc.xpath('//p[@class="ptsFin"]')[0] #オークションが終了かチェック
-        logger.debug("Get Html body")
-        quantity = 0
-        expired = false
-        if auction == nil then
-          logger.debug('Item is on sale')
-          title = doc.xpath('//h1[@class="ProductTitle__text"]')[0].inner_text
-          title = title.strip
-          tc = doc.xpath('//div[@class="Price Price--current"]')[0]
 
-          if tc != nil then
-            cPrice = tc.xpath('dl/dd[@class="Price__value"]/text()')[0]
-            cPrice = cPrice.inner_text.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-          else
-            cPrice = 0
-          end
+      header = sku.slice(0,3)
+      sku = sku.slice(3, -1)
+      if header == 'ya-' then
 
-          tb = doc.xpath('//div[@class="Price Price--buynow"]')[0]
-          if tb != nil then
-            bPrice = tb.xpath('dl/dd[@class="Price__value"]/text()')[0]
-            bPrice = bPrice.inner_text.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-          else
-            bPrice = 0
-          end
+        url = 'https://page.auctions.yahoo.co.jp/jp/auction/' + sku
+        logger.debug(url)
+        charset = nil
+        rt = rand(10)*0.1+0.2
+        sleep(rt)
+        begin
+          logger.debug("Access URL")
+          request = Typhoeus::Request.new(url)
+          request.run
+          html = request.response.body
+          doc = Nokogiri::HTML.parse(html, nil, charset)
+          isValid = true
+          auction = doc.xpath('//p[@class="ptsFin"]')[0] #オークションが終了かチェック
+          logger.debug("Get Html body")
+          quantity = 0
+          expired = false
+          if auction == nil then
+            logger.debug('Item is on sale')
+            title = doc.xpath('//h1[@class="ProductTitle__text"]')[0].inner_text
+            title = title.strip
+            tc = doc.xpath('//div[@class="Price Price--current"]')[0]
 
-          bit = doc.xpath('//li[@class="Count__count"]')[0]
-          if bit != nil then
-            bit = bit.xpath('dl/dd[@class="Count__number"]')[0].inner_text.to_i
-          else
-            bit = 0
-          end
+            if tc != nil then
+              cPrice = tc.xpath('dl/dd[@class="Price__value"]/text()')[0]
+              cPrice = cPrice.inner_text.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+            else
+              cPrice = 0
+            end
 
-          rest = doc.xpath('//li[@class="Count__count Count__count--sideLine"]')[0]
-          if rest != nil then
-            restunit = rest.xpath('dl/dd[@class="Count__number"]/span[@class="Count__unit"]')[0].inner_text
-            rest = rest.xpath('dl/dd[@class="Count__number"]/text()')[0].inner_text
+            tb = doc.xpath('//div[@class="Price Price--buynow"]')[0]
+            if tb != nil then
+              bPrice = tb.xpath('dl/dd[@class="Price__value"]/text()')[0]
+              bPrice = bPrice.inner_text.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+            else
+              bPrice = 0
+            end
 
-            case restunit
-            when "日" then
-              rest = rest.to_i * 60 * 24
-            when "時間" then
-              rest = rest.to_i * 60
-            when "分" then
-              rest = rest.to_i
+            bit = doc.xpath('//li[@class="Count__count"]')[0]
+            if bit != nil then
+              bit = bit.xpath('dl/dd[@class="Count__number"]')[0].inner_text.to_i
+            else
+              bit = 0
+            end
+
+            rest = doc.xpath('//li[@class="Count__count Count__count--sideLine"]')[0]
+            if rest != nil then
+              restunit = rest.xpath('dl/dd[@class="Count__number"]/span[@class="Count__unit"]')[0].inner_text
+              rest = rest.xpath('dl/dd[@class="Count__number"]/text()')[0].inner_text
+
+              case restunit
+              when "日" then
+                rest = rest.to_i * 60 * 24
+              when "時間" then
+                rest = rest.to_i * 60
+              when "分" then
+                rest = rest.to_i
+              else
+                rest = 0
+              end
             else
               rest = 0
             end
-          else
+
+            if bit == 0 then
+              bcheck = true
+            else
+              bcheck = false
+            end
+
+            if rest != 0 then
+              rcheck = true
+            else
+              rcheck = false
+            end
+            quantity = 1
+          else #オークションが終了の場合
+            logger.debug("Auction is end")
+            isValid = false
+            cPrice = doc.xpath('//tr[@class="elAucPriceRw"]')[0]
+            if cPrice != nil then
+              taxin = cPrice.xpath('//p[@class="decTxtTaxIncPrice"]')[0].inner_text
+              if taxin == '（税0円）'  then
+                cPrice = cPrice.xpath('//p[@class="decTxtAucPrice"]')[0].inner_text
+                cPrice = cPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+              else
+                cPrice = cPrice.xpath('//p[@class="decTxtAucPrice"]')[0].inner_text
+                cPrice = cPrice[3..(cPrice.length-2)]
+                cPrice = cPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+              end
+            else
+              cPrice = 0
+            end
+
+            bPrice = doc.xpath('//tr[@class="elBidOrBuyPriceRw"]')[0]
+            if bPrice != nil then
+              taxin = bPrice.xpath('//p[@class="decTxtTaxIncPrice"]')[0].inner_text
+              if taxin == '（税0円）'  then
+                bPrice = bPrice.xpath('//p[@class="decTxtBuyPrice"]')[0].inner_text
+                bPrice = bPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+              else
+                bPrice = bPrice.xpath('//p[@class="decTxtBuyPrice"]')[0].inner_text
+                bPrice = bPrice[3..(bPrice.length-2)]
+                bPrice = bPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
+              end
+            else
+              bPrice = 0
+            end
+
+            bit = doc.xpath('//b[@property="auction:Bids"]')[0].inner_text.to_i
             rest = 0
-          end
-
-          if bit == 0 then
-            bcheck = true
-          else
-            bcheck = false
-          end
-
-          if rest != 0 then
-            rcheck = true
-          else
+            if bit == 0 then
+              bcheck = true
+            else
+              bcheck = false
+            end
             rcheck = false
           end
-          quantity = 1
-        else #オークションが終了の場合
-          logger.debug("Auction is end")
+
+        rescue => e
+          logger.debug("Error!!\n")
+          logger.debug(e)
           isValid = false
-          cPrice = doc.xpath('//tr[@class="elAucPriceRw"]')[0]
-          if cPrice != nil then
-            taxin = cPrice.xpath('//p[@class="decTxtTaxIncPrice"]')[0].inner_text
-            if taxin == '（税0円）'  then
-              cPrice = cPrice.xpath('//p[@class="decTxtAucPrice"]')[0].inner_text
-              cPrice = cPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-            else
-              cPrice = cPrice.xpath('//p[@class="decTxtAucPrice"]')[0].inner_text
-              cPrice = cPrice[3..(cPrice.length-2)]
-              cPrice = cPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-            end
-          else
-            cPrice = 0
-          end
 
-          bPrice = doc.xpath('//tr[@class="elBidOrBuyPriceRw"]')[0]
-          if bPrice != nil then
-            taxin = bPrice.xpath('//p[@class="decTxtTaxIncPrice"]')[0].inner_text
-            if taxin == '（税0円）'  then
-              bPrice = bPrice.xpath('//p[@class="decTxtBuyPrice"]')[0].inner_text
-              bPrice = bPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-            else
-              bPrice = bPrice.xpath('//p[@class="decTxtBuyPrice"]')[0].inner_text
-              bPrice = bPrice[3..(bPrice.length-2)]
-              bPrice = bPrice.gsub(/\,/,"").gsub(/円/,"").gsub(/ /,"").to_i
-            end
-          else
-            bPrice = 0
-          end
-
-          bit = doc.xpath('//b[@property="auction:Bids"]')[0].inner_text.to_i
+          cPrice = 0
+          bPrice = 0
+          bit = 0
           rest = 0
-          if bit == 0 then
-            bcheck = true
-          else
-            bcheck = false
-          end
+          bcheck = false
           rcheck = false
+          expired = true
+        end
+      elsif header == 'me-' then
+        url = 'https://item.mercari.com/jp/' + sku
+        logger.debug(url)
+
+        begin
+          logger.debug("Access URL")
+          request = Typhoeus::Request.new(url)
+          request.run
+          html = request.response.body
+          doc = Nokogiri::HTML.parse(html, nil, charset)
+          isValid = true
+          title =  doc.xpath('//h2[@class="item-name"]')[0].inner_text
+          price = doc.xpath('//span[@class="item-price bold"]')[0].inner_text
+          price = price.gsub(/\,/,"").gsub(/\\/,"").gsub(/ /,"").to_i
+
+        rescue => e
+          logger.debug("Error!!\n")
+          logger.debug(e)
+          isValid = false
+          cPrice = 0
+          bPrice = 0
+          bit = 0
+          rest = 0
+          bcheck = false
+          rcheck = false
+          expired = true
         end
 
-      rescue => e
-        logger.debug("Error!!\n")
-        logger.debug(e)
-        isValid = false
+      elsif header == 'su-' then
+        url = 'https://www.suruga-ya.jp/product/detail/' + sku
+        logger.debug(url)
 
-        cPrice = 0
-        bPrice = 0
-        bit = 0
-        rest = 0
-        bcheck = false
-        rcheck = false
-        expired = true
+        begin
+          logger.debug("Access URL")
+          request = Typhoeus::Request.new(url)
+          request.run
+          html = request.response.body
+          doc = Nokogiri::HTML.parse(html, nil, charset)
+          isValid = true
+        rescue => e
+          logger.debug("Error!!\n")
+          logger.debug(e)
+          isValid = false
+          cPrice = 0
+          bPrice = 0
+          bit = 0
+          rest = 0
+          bcheck = false
+          rcheck = false
+          expired = true
+        end
+
       end
+
+
       logger.debug('Start updating table')
 
       progress = ""
